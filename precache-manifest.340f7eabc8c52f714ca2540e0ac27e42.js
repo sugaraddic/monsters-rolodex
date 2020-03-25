0ce32d088f2a6e0e43b6b31d26c4aa46d941cae2
Justin <jlodigkeit@frontsteps.com>
2020-03-25 17:38:51 -0600

    Updates

---
+++ precache-manifest.340f7eabc8c52f714ca2540e0ac27e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6365db383c4fc78c8986c3dcae0398",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f43c82a62c3452d4b2b2",
+    "url": "/monsters-rolodex/static/css/main.42b30d66.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f43c82a62c3452d4b2b2",
+    "url": "/monsters-rolodex/static/js/main.24854f13.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
